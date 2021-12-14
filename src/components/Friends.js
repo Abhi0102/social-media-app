102cd66e475a59a4a5183afa49479951070a93c4
Abhi0102 <abhishek1sharma14.1998@gmail.com>
2021-12-14 19:54:43 +0530

    Friend List

--- src/components/Friends.js
+++ src/components/Friends.js
@@ -6,8 +6,8 @@ function Friends(props) {
   return (
     //`user/${props.friend._id}`}
     <div>
-      <button onClick={() => console.log(props)}></button>
-      {/* <Link to="/">{props.friend.name}</Link> */}
+      {/* <button onClick={() => console.log(props)}></button> */}
+      <Link to={`/user/${props.friend._id}`}>{props.friend.name}</Link>
       {/* <h3>Friends</h3>
       </div>
       <div>
